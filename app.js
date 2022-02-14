083792634935f54b5a66edc7a8e1249397ebc49c
Kamasah DIckson <86136379+Kamasah-Dickson@users.noreply.github.com>
2022-02-14 02:51:19 +0000

    Add files via upload
    initialcommit

---
+++ app.js
@@ -0,0 +1,7 @@
+const nav = document.getElementById("nav-link")
+const toggle = document.getElementById("toggle")
+
+
+toggle.addEventListener("click", () =>{
+    nav.classList.toggle("active-nav")
+})
